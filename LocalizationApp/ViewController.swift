46d8f37537b9b985515705f4b80a0c52533ff657
ngodangtan <ngodangtan1994@gmail.com>
2021-09-06 22:39:15 +0700

    Initial Commit

---
+++ LocalizationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalizationApp
+//
+//  Created by Ngo Dang tan on 06/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
